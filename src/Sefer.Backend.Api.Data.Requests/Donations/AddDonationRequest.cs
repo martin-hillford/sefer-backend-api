aea712ae0bd0b7f518da1c391957240dedc75fa8
martin-hillford <171671237+martin-hillford@users.noreply.github.com>
2025-06-06 12:52:45 +0300

    Moved donations to a separate web api

--- src/Sefer.Backend.Api.Data.Requests/Donations/AddDonationRequest.cs
+++
@@ -1,6 +0,0 @@
-namespace Sefer.Backend.Api.Data.Requests.Donations;
-
-public class AddDonationRequest(Donation donation) : IRequest<bool>
-{
-    public readonly Donation Donation = donation;
-}
--- src/Sefer.Backend.Api.Data.Requests/Donations/SetDonationStateRequest.cs
+++
@@ -1,8 +0,0 @@
-namespace Sefer.Backend.Api.Data.Requests.Donations;
-
-public class SetDonationStateRequest(string donationId, PaymentStatus status) : IRequest<bool>
-{
-    public readonly string DonationId = donationId;
-
-    public readonly PaymentStatus Status = status;
-}
